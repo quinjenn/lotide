78235c83b7996d067e793ed99da26a352398bc52
quinjenn <jquintal_@hotmail.com>
2023-01-11 15:27:25 -0800

    adding assertArraysEqual.js assignment

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (var i = 0; i < firstArray.length; i++) {
+    if (
+      typeof firstArray[i] !== typeof secondArray[i] ||
+      firstArray[i] !== secondArray[i]
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} is the same as ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} is not the same as ${arr2}`);
+  }
+};
+
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
