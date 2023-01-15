153b8f611fdbf65fee26bbceef12a081b6ac0993
quinjenn <jquintal_@hotmail.com>
2023-01-14 18:24:27 -0800

    updated code

--- eqObjects.js
+++ eqObjects.js
@@ -25,32 +25,27 @@ const assertEqual = function (actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  console.log(
-    "objects:",
-    Object.keys(object1).length,
-    Object.keys(object2).length
-  );
-  const objectOneKeys = Object.keys(object1);
-  if (objectOneKeys.length !== Object.keys(object2).length) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
     return false;
   }
 
-  for (let obj1 of objectOneKeys) {
-    if (
-      Array.isArray(object1[obj1]) == true &&
-      Array.isArray(object2[obj1]) == true
-    ) {
-      if (eqArrays(object1[obj1], object2[obj1]) !== true) {
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else {
-      if (object1[obj1] !== object2[obj1]) {
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
   }
   return true;
 };
+
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
 eqObjects(shirtObject, anotherShirtObject); // => true
