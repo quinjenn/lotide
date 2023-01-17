abc3d87360b10ebeebec5fe8a7b9e7bd67fd8dc8
quinjenn <jquintal_@hotmail.com>
2023-01-17 09:29:52 -0800

    updating files

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
+const assertEqual = require("./assertEqual");
+
 let actual = "";
 let expected = "";
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function (array) {
   if (array.length > 0) {
@@ -16,5 +10,5 @@ const head = function (array) {
     return undefined;
   }
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
