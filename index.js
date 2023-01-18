6af9e1420bf7d18621c82ddb0a9743ae399719d2
quinjenn <jquintal_@hotmail.com>
2023-01-18 15:24:43 -0800

    final updates on lotide project

--- index.js
+++ index.js
@@ -2,15 +2,35 @@
 const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
-const head = require("./assertArraysEqual");
-const tail = require("./assertEqual");
-const middle = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
 };
